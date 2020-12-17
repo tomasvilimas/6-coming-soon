6d00cfdc6ee882f5883206aafd018723b9739c0e
tomasvilimas <74533072+tomasvilimas@users.noreply.github.com>
2020-12-17 19:04:33 +0200

    Render Social

---
+++ js/data/socialsData.js
@@ -0,0 +1,48 @@
+const socialsData = [
+
+    {
+        href: '#',
+        icon: 'facebook',
+
+    },
+
+    {
+        href: '#',
+        icon: 'twitter',
+
+    },
+
+    {
+        href: '#',
+        icon: 'dribbble',
+
+    },
+
+    {
+        href: '#',
+        icon: 'tumblr',
+
+    },
+
+    {
+        href: '#',
+        icon: 'linkedin',
+
+    },
+
+    {
+        href: '#',
+        icon: 'instagram',
+
+    },
+
+    {
+        href: '#',
+        icon: 'rss',
+
+    },
+
+
+];
+
+export {socialsData}
