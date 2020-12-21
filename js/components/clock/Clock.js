fcf254492b0dd1ebef06c4d56fa2ceb68c680a4f
tomasvilimas <74533072+tomasvilimas@users.noreply.github.com>
2020-12-21 22:03:04 +0200

    Clock shows Nan

---
+++ js/components/clock/Clock.js
@@ -0,0 +1,114 @@
+class Clock {
+
+    constructor(selector) {
+        this.selector= selector;
+        this.now = Date.now();
+    
+        this.deadline={
+            year: (new Date()).getFullYear(),
+            month:11,
+            day:16,
+            hour: 10,
+            minutes: 0,
+            seconds: 0,
+
+
+        }
+        
+            this.dateString = this.createDateString();
+            this.DOM= null;
+            this.DOMdays= null;
+            this.DOMhour= null;
+            this.DOMminutes= null;
+            this.DOMseconds= null;
+
+            
+           
+    }
+
+
+
+    
+
+        createDateString() {
+            const { year, month, day, hour, minutes, seconds }= this.deadline;
+            return `${year}-${month}-${day}-${hour}-${minutes}-${seconds}`;
+        }
+
+        ArBusMetines() {
+
+            const jubiliejausMiliseconds= (new Date(this.dateString)).getTime();
+            return this.now < jubiliejausMiliseconds;
+        }
+
+        atnaujintiInformacija() {
+
+            this.deadline.year++;
+            this.dateString =this.createDateString();
+        }
+
+        isValidSelector() {
+            const DOM = document.querySelector(this.selector);
+            if (!DOM) {
+                console.error('ERROR: blogas selectorius.')
+                return false;
+            }
+            this.DOM = DOM;
+            return true;
+        }
+
+        findClockValueElements(){
+            
+            const values =this.DOM.querySelectorAll('.value');
+            this.DOMdays= values[0];
+            this.DOMhour= values[1];
+            this.DOMminutes= values[2];
+            this.DOMseconds= values[3];
+
+
+        }
+
+        start(){
+            const jubiliejausMiliseconds= (new Date(this.dateString)).getTime();
+
+            setInterval(() => {
+
+                const now= new Date();
+                const diff= jubiliejausMiliseconds- now;
+
+                let secondsLeft = Math.floor (diff / 1000)
+                const days= Math.floor(secondsLeft /60 /60 /24);
+
+                secondsLeft-= days* 60 * 60 *24;
+                const hours= Math.floor (secondsLeft/ 60 / 60);
+
+                
+
+
+
+
+
+            this.DOMdays.innerText= days;
+            this.DOMhour.innerText= hours;
+            this.DOMminutes.innerText= now.getMinutes();
+            this.DOMseconds.innerText= now.getSeconds();
+
+
+                
+            }, 1000);
+
+        }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+export { Clock}
--- js/main.js
+++ js/main.js
@@ -1,9 +1,30 @@
+import{ Clock} from  './components/clock/Clock.js'
+
 import { progressBarData} from "./data/progressBarData.js";
 import{ renderProgressBar } from "./components/progress-bar/renderProgressBar.js";
 
 import {socialsData} from "./data/socialsData.js";
 import {renderSocials} from "./components/socials/renderSocials.js"
 
+
+const clock = new Clock('.hero .clock');
+
+if( clock.isValidSelector()) {
+        if(!clock.ArBusMetines()) {
+            clock.atnaujintiInformacija(); 
+}
+
+clock.findClockValueElements();
+}
+
+clock.start();
+
+
+console.log(clock);
+
+
 renderProgressBar('.left', progressBarData);
 
 renderSocials ('footer> .row-long' , socialsData);
+
+
